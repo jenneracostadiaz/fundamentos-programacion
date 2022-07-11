a1a066dc46cbb0b6f5dcb04ef10a3d72fe3ddf21
JennerDev <jacosta@markethinkers.pe>
2022-07-10 22:58:08 -0500

    v1.0.1

---
+++ seccion-4.4/app.js
@@ -0,0 +1,8 @@
+let gasolina = 3;
+while (gasolina > 0) {
+  // gasolina = gasolina - 1
+  gasolina--;
+  console.log("Gasolina restante " + gasolina);
+}
+
+console.log("Ya no tiene gasolina");
---
+++ seccion-4.5/app.js
@@ -0,0 +1,7 @@
+let base = 3;
+let limite = 12;
+
+for (let i = 1; i <= limite; i++) {
+  let resultado = base * i;
+  console.log(base + " x " + i + " = " + resultado);
+}
